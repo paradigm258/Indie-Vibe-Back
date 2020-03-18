abda2a2a4915f7d4684a6e828b9b75a432ac8c45
chanchanman <tuandt66742@gmail.com>
2020-03-18 10:31:23 +0700

    Add role field to user-profile to distinct users.

--- src/main/java/com/swp493/ivb/common/user/DTOUserPublic.java
+++ src/main/java/com/swp493/ivb/common/user/DTOUserPublic.java
@@ -3,6 +3,8 @@ package com.swp493.ivb.common.user;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.swp493.ivb.common.mdata.DTORole;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,8 +21,10 @@ public class DTOUserPublic {
     protected String thumbnail;
 
     protected int followersCount;
-    
+
     protected Set<String> relation = new HashSet<>();
 
     protected String type = "profile";
+
+    protected DTORole role;
 }
