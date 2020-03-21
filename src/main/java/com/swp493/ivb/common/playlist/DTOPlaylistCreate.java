50781550753fdcd0ca02688575c90caeda262d11
unknown <phamduythinh258@gmail.com>
2020-03-22 05:59:34 +0700

    various

--- src/main/java/com/swp493/ivb/common/playlist/DTOPlaylistCreate.java
+++ src/main/java/com/swp493/ivb/common/playlist/DTOPlaylistCreate.java
@@ -1,7 +1,6 @@
 package com.swp493.ivb.common.playlist;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.web.multipart.MultipartFile;
 
