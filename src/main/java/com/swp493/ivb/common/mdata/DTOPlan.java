1241a2ce639c3dafb0c21a9ab519212fd925326c
chanchanman <tuandt66742@gmail.com>
2020-03-28 11:50:52 +0700

    Add plan and plan-due info

---
+++ src/main/java/com/swp493/ivb/common/mdata/DTOPlan.java
@@ -0,0 +1,15 @@
+package com.swp493.ivb.common.mdata;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DTOPlan {
+
+    private String id;
+
+    private String name;
+}
--- src/main/java/com/swp493/ivb/common/user/DTOUserPrivate.java
+++ src/main/java/com/swp493/ivb/common/user/DTOUserPrivate.java
@@ -1,5 +1,7 @@
 package com.swp493.ivb.common.user;
 
+import com.swp493.ivb.common.mdata.DTOPlan;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +18,10 @@ public class DTOUserPrivate extends DTOUserPublic {
     private int gender;
 
     private String dob;
+
+    private String planDue;
+
+    private DTOPlan userPlan;
 
     private String artistStatus;
 }
