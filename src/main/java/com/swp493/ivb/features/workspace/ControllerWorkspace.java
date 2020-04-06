fa79c13184bb3f2f14c9b4205c5eb862e8bb6818
chanchanman <tuandt66742@gmail.com>
2020-04-06 17:38:08 +0700

    Miscall delete release on delete track

--- src/main/java/com/swp493/ivb/features/workspace/ControllerWorkspace.java
+++ src/main/java/com/swp493/ivb/features/workspace/ControllerWorkspace.java
@@ -88,7 +88,7 @@ public class ControllerWorkspace {
     
     @DeleteMapping(value = "/workspace/tracks/{id}")
     public ResponseEntity<?> deleteTrack(@PathVariable String id, @RequestAttribute EntityUser user){  
-        return Payload.successResponse("Release deleted: "+workspaceService.deleteRelease(user.getId(), id));
+        return Payload.successResponse("Release deleted: "+workspaceService.deleteTrack(user.getId(), id));
     }
 
     @PutMapping(value="/workspace/tracks/{id}")
