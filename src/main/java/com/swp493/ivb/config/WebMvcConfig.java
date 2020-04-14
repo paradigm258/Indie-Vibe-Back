f3b46aaea3aa3f8f15a5f4263c2e4488cf3db1ce
unknown <phamduythinh258@gmail.com>
2020-04-14 09:14:29 +0700

    cache config

--- src/main/java/com/swp493/ivb/config/WebMvcConfig.java
+++ src/main/java/com/swp493/ivb/config/WebMvcConfig.java
@@ -4,21 +4,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer  {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addViewController("/{spring:\\w+}")
-            .setViewName("forward:/");
-      registry.addViewController("/**/{spring:\\w+}")
-            .setViewName("forward:/");
-      registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-            .setViewName("forward:/");
-  }
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").setCachePeriod(600);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}").setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}").setViewName("forward:/");
+    }
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("api", HandlerTypePredicate.forAnnotation(RestController.class));
