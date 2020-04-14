db0b5411d8e517140204a6b2bc33b9fd2ea33925
unknown <phamduythinh258@gmail.com>
2020-04-14 10:00:41 +0700

    remove cors

--- src/main/java/com/swp493/ivb/config/IndieAuthConfiguration.java
+++ src/main/java/com/swp493/ivb/config/IndieAuthConfiguration.java
@@ -1,7 +1,5 @@
 package com.swp493.ivb.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +11,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -40,7 +35,6 @@ public class IndieAuthConfiguration extends WebSecurityConfigurerAdapter {
         http
             .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            // .and().cors()
             .and().csrf().disable()
             .exceptionHandling()
             .authenticationEntryPoint(unauthorizedHandler)
@@ -51,17 +45,5 @@ public class IndieAuthConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.POST, "/releases").hasAuthority("r-artist")
             .and().logout().disable();
     }
-
-    // @Bean
-    // public CorsConfigurationSource corsConfigurationSource() {
-    //     CorsConfiguration configuration = new CorsConfiguration();
-    //     configuration.setAllowedOrigins(Arrays.asList("*"));
-    //     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    //     configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "Upgrade-Insecure-Requests", "range"));
-    //     configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     source.registerCorsConfiguration("/**", configuration);
-    //     return source;
-    // }
 
 }
