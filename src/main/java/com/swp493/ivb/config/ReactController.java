980301fa767a3e6eb657419ae763d5df34f97dd9
unknown <phamduythinh258@gmail.com>
2020-04-14 11:20:52 +0700

    redirect whitelabel

--- src/main/java/com/swp493/ivb/config/ReactController.java
+++ src/main/java/com/swp493/ivb/config/ReactController.java
@@ -1,14 +1,28 @@
 package com.swp493.ivb.config;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-public class ReactController {
+public class ReactController implements ErrorController{
+    private static final String PATH = "/error";
+
     @GetMapping(value="/")
     public ModelAndView getMethodName() {
         return new ModelAndView("index.html");
+    }
+
+    @RequestMapping(value = PATH)
+    public ModelAndView error() {
+        return new ModelAndView("index.html");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
     }
 }
